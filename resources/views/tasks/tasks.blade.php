d1359f8cf4bece03ff21c73d4951c6ec4dc8a659
Rohringer Zoltan <rohringer.zoltan@gmail.com>
2023-06-27 10:31:28 +0200

    feat: tasks page styling

--- resources/views/tasks/tasks.blade.php
+++ resources/views/tasks/tasks.blade.php
@@ -5,21 +5,20 @@
 <p>No Task found.</p>
 @endif
 
-<form method="GET" action="/tasks">
-    @csrf
-    <input type="hidden" name="show_archived" value="{{ !$showArchived }}">
-    <button>{{ $buttonLabel }}</button>
-</form>
+<div class="flex flex-row justify-between mr-8 ml-8 mb-4">
+    <form method="GET" action="/tasks">
+        @csrf
+        <input type="hidden" name="show_archived" value="{{ !$showArchived }}">
+        <button class="bg-blue-600 text-black py-2 px-5">{{ $buttonLabel }}</button>
+    </form>
+    <p class="font-bold uppercase py-2 px-5">Tasks</p>
+    <a href="/tasks/create" class="bg-blue-600 text-black py-2 px-5">Add Task</a>
+</div>
+<div class="flex flex-wrap justify-evenly">
 @foreach($tasks as $task)
     <div class="p-3 md:grid gap-4 border-2 border-black">
-        <h2>{{$task['name']}}</h2>
-        <p>{{$task['description']}}</p>
-        <form method="POST" action="/tasks/{{$task['id']}}">
-            @csrf
-            @method('DELETE')
-            <button>Delete</button>
-        </form>
-        <a href="/tasks/{{$task['id']}}/edit">Edit</a>
+        <h2>Name: {{$task['name']}}</h2>
+        <p>Description: {{$task['description']}}</p>
         <form method="POST" action="/tasks/{{ $task->id }}/status" id="status-form">
             @csrf
             @method('PUT')
@@ -35,16 +34,21 @@
 
             <button type="submit" style="display: none;">Update Status</button>
         </form>
-
-        <form method="POST" action="/tasks/{{$task['id']}}/archive">
-            @csrf
-            @method('PUT')
-            <button>{{ $task['isArchived'] ? 'Activate' : 'Archive' }}</button>
-        </form>
-
+        <div class="flex flex-row justify-evenly">
+            <form method="POST" action="/tasks/{{$task['id']}}">
+                @csrf
+                @method('DELETE')
+                <button class="mr-4 bg-blue-600 text-black py-2 px-5">Delete</button>
+            </form>
+            <a class="mr-4 bg-blue-600 text-black py-2 px-5" href="/tasks/{{$task['id']}}/edit">Edit</a>
+            <form method="POST" action="/tasks/{{$task['id']}}/archive">
+                @csrf
+                @method('PUT')
+                <button class="mr-4 bg-blue-600 text-black py-2 px-5">{{ $task['isArchived'] ? 'Activate' : 'Archive' }}</button>
+            </form>
+        </div>
     </div>
 @endforeach
-<a href="/tasks/create" class="right-10 bg-black text-white py-2 px-5">Add Task</a>
-
+</div>
 
 @endsection
